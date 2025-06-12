87274acc17a9e544a28c3d4e61ebd54aab36cb4c
Hyogyeong Shin <haydenhyo@gmail.com>
2025-06-12 12:30:56 +0900

    [Silver II] Title: 최대 힙, Time: 156 ms, Memory: 114208 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,12 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+heap = []
+n = int(input().rstrip())
+for _ in range(n):
+  i = int(input().rstrip())
+  if i == 0:
+    print(abs(heapq.heappop(heap)) if heap else 0)
+  else:
+    heapq.heappush(heap, -i)
