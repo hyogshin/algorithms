98ff413e8e7e392a25d1514f168403078cb7d5da
Hyogyeong Shin <haydenhyo@gmail.com>
2025-08-28 00:06:34 +0700

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ BOJ/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,12 @@
+pixels = [list(input()) for _ in range(15)]
+
+for raw in range(15):
+  if 'w' in pixels[raw]:
+    print('chunbae')
+    break
+  elif 'b' in pixels[raw]:
+    print('nabi')
+    break
+  elif 'g' in pixels[raw]:
+    print('yeongcheol')
+    break
