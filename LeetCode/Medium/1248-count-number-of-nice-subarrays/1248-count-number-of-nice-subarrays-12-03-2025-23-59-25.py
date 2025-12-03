30e0dc9d6b47a1cb7e4bf924ff6a780b3c0ea792
Hayden Shin <haydenhyo@gmail.com>
2025-12-03 23:59:26 +0000

    Time: 83 ms (75.46%), Space: 24.6 MB (29.38%) - LeetHub

---
+++ LeetCode/Medium/1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays-12-03-2025-23-59-25.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        prefix = {0: 1}
+        odds = 0
+        ans = 0
+        
+        for x in nums:
+            if x % 2:
+                odds += 1
+        
+            ans += prefix.get(odds - k, 0)
+            
+            prefix[odds] = prefix.get(odds, 0) + 1
+        
+        return ans
